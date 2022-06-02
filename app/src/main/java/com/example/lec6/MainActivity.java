a1925be1f458a9a9c80ec9c04d784218c14e7ada
BoomBakar <abubakardar608@gmail.com>
2022-06-02 08:48:14 +0500

    Toast added

--- app/src/main/java/com/example/lec6/MainActivity.java
+++ app/src/main/java/com/example/lec6/MainActivity.java
@@ -3,12 +3,32 @@ package com.example.lec6;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn.findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LayoutInflater layoutInflater = getLayoutInflater();
+                View myLayout = layoutInflater.inflate(R.layout.activity_main,null);
+                Toast myToast = new Toast(getApplicationContext());
+                myToast.setDuration(Toast.LENGTH_LONG);
+                myToast.setView(myLayout);
+                //myToast.setGravity(Gravity.CENTER);
+                
+
+            }
+        });
     }
 }
